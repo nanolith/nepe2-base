a1bb9d25a217a01247e45b95f5508418dbb4ce7c
Justin Handville <nanolith@gmail.com>
2023-06-22 03:40:51 -0400

    decl for metadata_legacy_flag_get.

--- include/nepe2/metadata.h
+++ include/nepe2/metadata.h
@@ -457,6 +457,28 @@ status FN_DECL_MUST_CHECK
 metadata_legacy_flag_set(
     metadata* meta, bool legacy_flag);
 
+/**
+ * \brief Get the legacy flag for a given \ref metadata instance.
+ *
+ * \param legacy_flag       Pointer to hold the legacy flag on success.
+ * \param meta              The metadata instance for this operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - ERROR_METADATA_FIELD_NOT_SET if the method fails because this field
+ *        has not been set.
+ *
+ * \pre
+ *      - \p legacy_flag must be a valid pointer.
+ *      - \p meta must reference a valid \ref metadata instance.
+ * \post
+ *      - On success, \p legacy_flag is set to the legacy_flag of this instance.
+ *      - On failure, \p legacy_flag is unchanged.
+ */
+status FN_DECL_MUST_CHECK
+metadata_legacy_flag_get(
+    bool* legacy_flag, const metadata* meta);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
