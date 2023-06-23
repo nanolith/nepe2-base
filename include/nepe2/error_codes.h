c29ef2f3ade02ecc9a625b01ccb1656736f229c1
Justin Handville <nanolith@gmail.com>
2023-06-23 02:19:57 -0400

    impl for metadata_hash_id_get.

---
+++ include/nepe2/error_codes.h
@@ -0,0 +1,12 @@
+/**
+ * \file nepe2/error_codes.h
+ *
+ * \brief Error codes for nepe library.
+ *
+ * \copyright 2023 Justin Handville.  Please see License.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#define ERROR_METADATA_FIELD_NOT_SET                                    0x3401
