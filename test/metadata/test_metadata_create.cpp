d19c3278f84c2af05ca982767aafdaa97a2a1238
Justin Handville <nanolith@gmail.com>
2023-07-23 15:52:45 -0400

    Added unit tests for metadata_hash_id_set/get.

--- test/metadata/test_metadata_create.cpp
+++ test/metadata/test_metadata_create.cpp
@@ -5,7 +5,9 @@
  */
 
 #include <minunit/minunit.h>
+#include <nepe2/error_codes.h>
 #include <nepe2/metadata.h>
+#include <string.h>
 
 RCPR_IMPORT_allocator;
 RCPR_IMPORT_resource;
@@ -31,6 +33,59 @@ TEST(basics)
 
     /* the metadata instance is empty. */
     TEST_EXPECT(metadata_empty_flag_get(meta));
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == resource_release(metadata_resource_handle(meta)));
+    TEST_ASSERT(
+        STATUS_SUCCESS == resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * We can set and get a hash id.
+ */
+TEST(metadata_hash_id_set_get)
+{
+    const uint8_t hash_id[] = {
+        0x5e, 0x5f, 0x4e, 0xfb, 0x2c, 0xd4, 0x4c, 0x21,
+        0x9b, 0x33, 0x05, 0xda, 0x5d, 0xb7, 0xd5, 0x65,
+        0x03, 0xeb, 0xc4, 0xe4, 0x5b, 0x95, 0x49, 0x12,
+        0xa2, 0x5f, 0x5f, 0x97, 0xc8, 0xf3, 0x03, 0x81 };
+    const size_t hash_id_size = sizeof(hash_id);
+    allocator* alloc = nullptr;
+    metadata* meta = nullptr;
+    const void* hptr = nullptr;
+    size_t hptr_size = 0U;
+
+    /* we can successfully create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* we can successfully create a metadata instance. */
+    TEST_ASSERT(STATUS_SUCCESS == metadata_create(&meta, alloc));
+
+    /* if we attempt to get the hash id before it is set, we get an error. */
+    TEST_EXPECT(
+        ERROR_METADATA_FIELD_NOT_SET
+            == metadata_hash_id_get(&hptr, &hptr_size, meta));
+
+    /* set the hash id. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == metadata_hash_id_set(meta, hash_id, hash_id_size));
+
+    /* we can now get the hash id. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == metadata_hash_id_get(&hptr, &hptr_size, meta));
+
+    /* the hash pointer is set by the getter. */
+    TEST_ASSERT(nullptr != hptr);
+
+    /* the size matches. */
+    TEST_ASSERT(hash_id_size == hptr_size);
+
+    /* the data matches. */
+    TEST_EXPECT(!memcmp(hptr, hash_id, hash_id_size));
 
     /* clean up. */
     TEST_ASSERT(
