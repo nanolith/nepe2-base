cb1fd65b664fd9fedff2b5154092e8ee5c78dc0e
Justin Handville <nanolith@gmail.com>
2026-09-22 11:17:25 -0400

    transformer_fn decl.

--- include/nepe2/transformer.h
+++ include/nepe2/transformer.h
@@ -30,6 +30,27 @@ extern "C" {
  */
 typedef struct transformer transformer;
 
+/**
+ * \brief A transformer function takes an allocator, a master passphrase, a
+ * session passphrase, and metadata to produce a secure buffer containing a
+ * raw generated password.
+ *
+ * \param raw           Pointer to the secure buffer pointer to be set to the
+ *                      raw generated password on success.
+ * \param alloc         The allocator to use for this operation.
+ * \param master        The master passphrase to use for this operation.
+ * \param session       The session passphrase to use for this operation.
+ * \param meta          The metadata to use for this operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+typedef status (*transformer_fn)(
+    secure_buffer** raw, RCPR_SYM(allocator)* alloc,
+    const secure_buffer* master, const secure_buffer* session,
+    const metadata* meta);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
