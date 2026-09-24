42507e16027d204bced306a221b9d211fde0dce7
Justin Handville <nanolith@gmail.com>
2026-09-23 22:43:02 -0400

    ERROR_TRANSFORMER_REGISTRY_CANNOT_BE_RELEASED decl.

--- include/nepe2/error_codes.h
+++ include/nepe2/error_codes.h
@@ -13,3 +13,4 @@
 #define ERROR_METADATA_BAD_ENCODING_LENGTH                              0x3402
 #define ERROR_METADATA_INVALID_BUFFER_SIZE                              0x3403
 #define ERROR_METADATA_UNKNOWN_SERIAL_VERSION                           0x3404
+#define ERROR_TRANSFORMER_REGISTRY_CANNOT_BE_RELEASED                   0x3405
